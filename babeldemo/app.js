e9a82ee11d159180e421452c3460605e24383652
= <=>
2020-08-30 23:58:15 -0400

    Commited with Spread Operator
    Co-Authored-By: Dilip <dilipdmane@gmail.com>

--- babeldemo/app.js
+++ babeldemo/app.js
@@ -1,6 +1,27 @@
-"use strict";
 function sayHi(humanName) {
-    alert("Hi " + humanName + " Compied with Typescript complier It's fun!  ");
-    console.log(humanName + "...");
+    var SMALL = 0;
+    var MEDIUM = 100;
+    var LARGE = 2;
+    var Size;
+    (function (Size) {
+        Size[Size["SMALL"] = 0] = "SMALL";
+        Size[Size["MEDIUM"] = 1] = "MEDIUM";
+        Size[Size["LARGE"] = 2] = "LARGE";
+    })(Size || (Size = {}));
+    ;
+    var a;
+    a = ["50", "55"];
+    console.log(concate("Hi " + humanName + "...", "Compiled with Typescript complier It's fun! "));
+    var myAppSize;
+    myAppSize = Size.LARGE;
+    var person = { firstName: "Dilip", lastName: "Mane", age: 52 };
+    console.log(person);
+    // Addd Spread operator
+    var addNumber = function (num1, num2) { return num1 + num2; };
+    var nums = [5, 5];
+    console.log(addNumber.apply(void 0, nums));
+}
+function concate(string1, string2) {
+    return string1 + string2;
 }
 sayHi('React ');
--- babeldemo/app.ts
+++ babeldemo/app.ts
@@ -1,7 +1,30 @@
 function sayHi(humanName:string)
 {
-    alert(`Hi ${humanName} Compiled with Typescript complier It's fun!  `)
-    console.log(`${humanName}...`)
+  const SMALL = 0;
+  const MEDIUM = 100;
+  const LARGE = 2;
+  enum Size {SMALL,MEDIUM,LARGE};
+   let a:string[] ;
+   a=["50","55"];
+    console.log(concate(`Hi ${humanName}...`,`Compiled with Typescript complier It's fun! `));
+    let myAppSize:Size;
+    myAppSize = Size.LARGE;
+    //alert(myAppSize);
+
+    // Understand object type definitions
+    type personType={firstName:string,lastName:string,age:number};
+    let person:personType={firstName:"Dilip",lastName:"Mane",age:52};
+    console.log(person);
+
+    // Addd Spread operator
+    const addNumber = (num1?: number,num2?: number):number=>num1+num2;
+    const nums:number[] = [5,5];
+    console.log( addNumber(...nums));
+
+}
+
+function concate( string1:string, string2:string):string{
+  return string1+string2;
 }
 
 sayHi( 'React ');
