0d1e42f4c32117dc5e232a7f285ad2d26d8de772
jyodak <jyodak@jyodak-HP-Pavilion-Notebook>
2020-11-12 01:04:25 +0530

    Tested

--- MicroServices/starterapp/src/main/java/com/vibhuti/basic/domain/core/service/ReadOnlyBaseService.java
+++ MicroServices/starterapp/src/main/java/com/vibhuti/basic/domain/core/service/ReadOnlyBaseService.java
@@ -23,4 +23,6 @@ public abstract class ReadOnlyBaseService<TE,T> {
 	}
 	
 	
+	
+	
 }
--- MicroServices/starterapp/src/main/java/com/vibhuti/basic/starterapp/App.java
+++ MicroServices/starterapp/src/main/java/com/vibhuti/basic/starterapp/App.java
@@ -1,5 +1,16 @@
 package com.vibhuti.basic.starterapp;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import com.vibhuti.basic.domain.InMemRestaurantRepository;
+import com.vibhuti.basic.domain.Restaurant;
+import com.vibhuti.basic.domain.RestaurantService;
+import com.vibhuti.basic.domain.Table;
+
 /**
  * Hello world!
  *
@@ -8,6 +19,35 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {        	
+        	//Initialize restaurant service
+        	RestaurantService restaurantService = new RestaurantService(new InMemRestaurantRepository());
+        	
+        	//Add tables
+        	
+        	List<Table> tables = Arrays.asList(
+        			new Table(BigInteger.ONE, "Table 1", 5),
+        			new Table(BigInteger.valueOf(2), "Table 2", 6),
+        			new Table(BigInteger.valueOf(3), "Table 3", 7));
+        	
+        	// Add few restaurants using Service
+            // Note: To raise an exception give same restaurant name to one of the below restaurant
+            restaurantService
+                .add(new Restaurant("1","Big-O Restaurant",  tables));
+            restaurantService.add(new Restaurant("2","Pizza Shops", new ArrayList<Table>() ));
+            restaurantService.add(new Restaurant("3","La Pasta",  new ArrayList<Table>()));
+            
+            // Retrieving all restaurants using Service
+            Collection<Restaurant> restaurants = restaurantService.getAll();
+            
+            System.out.println("App.main() Restaurant List ...");
+            
+            restaurants.stream().map(r -> String.format("Restaurant : %s", r)).forEach(
+            		System.out::println
+            		);
+			
+		} catch (Exception e) {
+			 System.out.println(String.format("Exception: %s", e.getMessage()));
+		}
     }
 }
