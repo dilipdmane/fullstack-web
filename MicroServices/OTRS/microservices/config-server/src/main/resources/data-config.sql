e94f65ddf02e74f95a65a40ce2e7496e68bbc8f9
Dilip Mane <dilipdmane@gmail.com>
2021-01-24 02:40:49 +0530

    Added Updates for compose

--- MicroServices/OTRS/microservices/config-server/src/main/resources/data-config.sql
+++ MicroServices/OTRS/microservices/config-server/src/main/resources/data-config.sql
@@ -1,8 +1,8 @@
 INSERT INTO properties (id, application, profile, label, key, value)
-VALUES (1, 'booking-service', 'jdbc', 'master', 'app.greet.msg', 'JDBC: Warm welcome from user microservice!');
+VALUES (1, 'booking-service', 'jdbc', 'master', 'app.greet.msg', 'JDBC: Warm welcome from user booking-service!');
 INSERT INTO properties (id, application, profile, label, key, value)
-VALUES (2, 'booking-service', 'docker', 'master', 'app.greet.msg', 'Docker: Warm welcome from user microservice!');
+VALUES (2, 'booking-service', 'docker', 'master', 'app.greet.msg', 'Docker: Warm welcome from user booking-service!');
 INSERT INTO properties (id, application, profile, label, key, value)
-VALUES (3, 'restaurantservice', 'docker', 'master', 'app.greet.msg', 'Docker: Warm welcome from user restaurantservice!');
+VALUES (3, 'restaurant-service', 'docker', 'master', 'app.greet.msg', 'Docker: Warm welcome from user restaurant-service!');
 INSERT INTO properties (id, application, profile, label, key, value)
-VALUES (4, 'restaurantservice', 'jdbc', 'master', 'app.greet.msg', 'Docker: Warm welcome from restaurantservice!');
+VALUES (4, 'restaurant-service', 'jdbc', 'master', 'app.greet.msg', 'Docker: Warm welcome from restaurant-service!');
